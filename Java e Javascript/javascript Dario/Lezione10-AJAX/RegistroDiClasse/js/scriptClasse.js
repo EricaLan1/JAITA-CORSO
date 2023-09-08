047a4324a6cb16ed6600fd83b6e9b2a3d79fc961
EricaLan1 <ericalandolfo1@gmail.com>
2023-09-08 17:23:28 +0200

    sdeguito

--- Java e Javascript/javascript Dario/Lezione10-AJAX/RegistroDiClasse/js/scriptClasse.js
+++ Java e Javascript/javascript Dario/Lezione10-AJAX/RegistroDiClasse/js/scriptClasse.js
@@ -1,7 +1,16 @@
+let listaStudenti = document.querySelector("#listaStudenti");
 const URL = "http://localhost:3000/studenti";
 
 fetch(URL)
 .then(data =>{return data.json()})
 .then(response =>{
     console.log(response);
+
+    response.forEach(studente => {
+        let card = document.createElement("li");
+        card.setAttribute("class","list-group-item");
+        card.textContent = `${studente.id} - ${studente.nome} - ${studente.cognome}`;
+
+        listaStudenti.appendChild(card);
+    });
 })
