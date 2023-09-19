0a1eb436bbd4278b501a82fabaacd5ea76f8341b
EricaLan1 <ericalandolfo1@gmail.com>
2023-09-19 12:05:13 +0200

    jiji

--- Java e Javascript/javascript Dario/Es_POKEDEX/js/pokedex.js
+++ Java e Javascript/javascript Dario/Es_POKEDEX/js/pokedex.js
@@ -1,5 +1,29 @@
 const datiMemorizzati = localStorage.getItem("pokemonScelti");
+let pokemonSelezionati = document.querySelector("#Pokedex");
+
+
 if (datiMemorizzati) {
     const scelta = JSON.parse(datiMemorizzati);
     console.log(scelta);
+}
+
+
+
+function card(params) {
+
+    pokedex.innerHTML = "";
+    
+
+let card = `
+    <div class="col-4 mt-5">
+        <div class="card">
+            <div class='card-body ${classBgCard}'>
+            <img class="card-img-top " src=${datiMemorizzati.url}>
+                <h3 class="card-title">${datiMemorizzati.name}</h3>
+              
+            </div>
+        </div>
+    </div>`;
+
+    listaDocenti.innerHTML += card;
 }
