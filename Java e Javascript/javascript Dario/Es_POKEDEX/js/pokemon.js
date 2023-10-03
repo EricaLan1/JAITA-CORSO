636bb95e45f6d316d89a1f091ab84258d5a054db
EricaLan1 <ericalandolfo1@gmail.com>
2023-10-03 16:10:16 +0200

    .

--- Java e Javascript/javascript Dario/Es_POKEDEX/js/pokemon.js
+++ Java e Javascript/javascript Dario/Es_POKEDEX/js/pokemon.js
@@ -1,20 +1,20 @@
 let URL = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0";
 let demo = document.querySelector("#demo");
-let btn=document.querySelector("#btn");
-let btnAllert= document.querySelector(".alert");
+let btn = document.querySelector("#btn");
+let btn1 = document.querySelector("#btn1");
+let collezione = document.querySelector("#collezione");
 
 fetch(URL)
-.then(data => {
-    return data.json();
-})
+    .then(data => {
+        return data.json();
+    })
 
-.then(Response => {
-    // console.log(Response.results[0].name);  
-    for (let i =0;i<Response.results.length;i++)
-        {
-        let card = document.createElement("div");
-        card.classList.add("col-md-4", "mb-3");
-        card.innerHTML = `
+    .then(Response => {
+        // console.log(Response.results[0].name);  
+        for (let i = 0; i < Response.results.length; i++) {
+            let card = document.createElement("div");
+            card.classList.add("col-md-4", "mb-3");
+            card.innerHTML = `
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">${Response.results[i].name}</h5>
@@ -22,52 +22,39 @@ fetch(URL)
                     </div>
             </div>
         `;
-        demo.appendChild(card);
-      
+            demo.appendChild(card);
+
         }
-       
-        
 
 
-    
-})
+
+
+
+    })
 
 
 let scelta = [];
-function pokemon(nome,url)
-{
 
-    if (scelta.length<=8)
-    {
-        scelta.push(nome,url)
+function pokemon(nome, url) {
+
+    if (scelta.length < 6) {
+        scelta.push(nome, url)
         console.log(scelta);
 
-        let spazioAlert = document.querySelector("#spazioAlert");
-
-        let alert = `
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Complimenti!</strong> Hai selezionato il tuo pokemon.
-                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>`;
-
-            spazioAlert.innerHTML = alert;
-
-    }
-    else
-    {
+    } else {
         alert("Non puoi più aggiungere pokemon")
     }
 
 
 }
 
-btn.addEventListener("click",pokemon);
+btn.addEventListener("click", pokemon);
 
-//
-btn.addEventListener("click", () => {
-    
-    if (scelta.length > 3) {
-        
+
+btn1.addEventListener("click", () => {
+
+    if (scelta.length >= 3) {
+
         localStorage.setItem("pokemonScelti", JSON.stringify(scelta));
 
         window.location.href = "pokedex.html";
@@ -75,6 +62,3 @@ btn.addEventListener("click", () => {
         alert("Devi selezionare almeno tre Pokémon.");
     }
 });
-
-
-//sdgsgsdgs
