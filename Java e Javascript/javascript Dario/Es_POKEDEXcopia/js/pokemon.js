91e36c45420febe4277e63d396a02a94f1138a89
EricaLan1 <ericalandolfo1@gmail.com>
2023-10-10 16:58:45 +0200

    .

--- Java e Javascript/javascript Dario/Es_POKEDEXcopia/js/pokemon.js
+++ Java e Javascript/javascript Dario/Es_POKEDEXcopia/js/pokemon.js
@@ -1,4 +1,4 @@
-let URL = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0";
+let URL = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0"; // api pokemon
 let demo = document.querySelector("#demo");
 let btn = document.querySelector("#btn");
 let btn1 = document.querySelector("#btn1");
@@ -7,14 +7,14 @@ let pokedex = [];
 let stampaGruppi = document.querySelector("#lista");
 
 
-function cartaPokemon(pokemon) {
+function cartaPokemon(pokemon) {  //creaiamo
     
     let card= document.createElement("li");
             card.classList.add("list-group-item");
             card.innerHTML = `
-                <img src="" >
-                <h6>${pokemon.name}</h6>
-                <button type="button" class="btn btn-primary bottoneAggiungi">Aggiungi al Pokedex</button>
+                <img src= >
+                <span>${pokemon.name}</span>
+                <button type="button" class="btn btn-primary bottoneAggiungi d-inline-block">Aggiungi al Pokedex</button>
             `;
 
     
@@ -94,62 +94,3 @@ listaPokemon();
 
 
 
-
-
-
-
-    // <div class="container mt-4">
-    //     <h1>Pokemon List</h1>
-    //     <ul id="pokemonList" class="list-group">
-    //     </ul>
-    // </div>
-
-    // 
-    //     let URL = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0";
-    //     let demo = document.querySelector("#pokemonList");
-    //     let pokedex = [];
-
-
-    //         let addButton = listItem.querySelector(".bottoneAggiungi");
-    //         addButton.addEventListener("click", () => {
-    //             if (pokedex.length < 3) {
-    //                 let pokemonName = pokemon.name;
-    //                 if (!pokedex.includes(pokemonName)) {
-    //                     pokedex.push(pokemonName);
-    //                     alert("Hai selezionato correttamente il tuo Pokémon!");
-    //                 } else {
-    //                     alert("Errore! Pokémon già selezionato.");
-    //                 }
-
-    //                 if (pokedex.length >= 3) {
-    //                     alert("Hai scelto correttamente i tre Pokémon");
-    //                     localStorage.setItem("pokemonScelti", JSON.stringify(pokedex));
-    //                 }
-    //             }
-    //         });
-
-    //         return listItem;
-    //     }
-
-    //     function listaPokemon() {
-    //         fetch(URL)
-    //             .then((data) => {
-    //                 return data.json();
-    //             })
-    //             .then((response) => {
-    //                 response.results.forEach((pokemon) => {
-    //                     fetch(pokemon.url)
-    //                         .then((data) => {
-    //                             return data.json();
-    //                         })
-    //                         .then((response) => {
-    //                             let listItem = elementoListaPokemon(response);
-    //                             demo.appendChild(listItem);
-    //                         });
-    //                 });
-    //             });
-    //     }
-
-    //     // Chiama la funzione per ottenere i dati dei Pokémon e creare gli elementi della lista
-    //     listaPokemon();
-   
