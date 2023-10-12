6cf4558d32a469599cc5f33f5976d90a783dca34
EricaLan1 <ericalandolfo1@gmail.com>
2023-10-12 16:56:39 +0200

    .

---
+++ Java e Javascript/javascript Dario/ProjectWork/script.js
@@ -0,0 +1,9 @@
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+  //da mobile nascondere la parte a destra lasciando solo i bottoni. aggiustare il css per centrare
