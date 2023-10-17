cdc3c6e95a8816802f477b61a6b8e741f3d5e3f4
EricaLan1 <ericalandolfo1@gmail.com>
2023-10-17 09:17:10 +0200

    .

--- Java e Javascript/javascript Dario/ProjectWork/js/prova3.js
+++ Java e Javascript/javascript Dario/ProjectWork/js/prova3.js
@@ -1,13 +1,14 @@
-function cambioBG(bg, titolo) {
-    let banner = document.querySelector('.banner');
-    let contenuti = document.querySelectorAll('.content');
-    banner.style.backgroundSize = 'cover';
-    banner.style.backgroundPosition = 'center';
+    function cambioBg(bg, titolo) {
+        let banner = document.querySelector('.banner');
+        let contenuti = document.querySelectorAll('.content');
+        banner.style.background = `url("../img/${bg}")`;
+        banner.style.backgroundSize = 'cover';
+        banner.style.backgroundPosition = 'center';
 
-    contenuti.forEach(contenuti =>{
-        contenuti.classList.remove('active');
-        if (contenuti.classList.contains(title)) {
-            contenuti.classList.add('active')
-        }
-    })
-}
+        contenuti.forEach(content =>{
+            content.classList.remove('active');
+            if (content.classList.contains(titolo)) {
+                content.classList.add('active')
+            }
+        });
+    }
